8f0a2dd4d089bc034e0c490bf8799b274d4b1e3c
陳君平 <daniel9563769@gmail.com>
2020-11-19 20:02:34 +0800

    2020/11/19

--- api/main.js
+++ api/main.js
@@ -374,9 +374,8 @@ app.post('/api/acceptFriend', function(req, res){
                     (select *\
                     from (select chat_id\
                         from friend\
-                        where user_id_other = \""+fid+"\" and user_id_self = \""+uid+"\") as A)";
-                   
-
+                        where user_id_self = \""+fid+"\" and user_id_other = \""+uid+"\") as A)";
+                
     con.query(addfriend,function(err,result){
         if (err) throw err;
         res.send("success");
