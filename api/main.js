82bf13fa5beee1e6d81497c4af8135fe4f519e54
陳君平 <daniel9563769@gmail.com>
2020-12-03 15:27:42 +0800

    12/3

--- api/main.js
+++ api/main.js
@@ -440,6 +440,7 @@ app.post('/api/rejectFriend', function(req, res){
 
 io.on('connection', (socket) => {
     socket.on("chat_info", (msg1) => {
+        console.log(msg1);
         socket.on("send", (msg2) => {
             if(msg1.chat_id == msg2.chat_id)
                 io.emit("msg", msg2);
