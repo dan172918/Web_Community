9db7d69a8d6ccc464cceb3c3f0ac07d586b8145d
OscarChang1380 <st94280@gmail.com>
2020-12-07 12:58:07 +0800

    12/7

--- Web/js/index.js
+++ Web/js/index.js
@@ -1,3 +1,27 @@
+window.onload = function() { 
+    setTimeout(function(){
+        $(".load").fadeIn(1500);
+        $(".load").fadeOut(1500);
+    },500)
+    
+    setTimeout(function(){
+      $(".waitloading").css("opacity","1");
+    },4000)
+};
+
+$(function(){
+	$('#BackTop').click(function(){ 
+		$('html,body').animate({scrollTop:0}, 333);
+	});
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 10 ){
+			$('#BackTop').fadeIn(10);
+		} else {
+			$('#BackTop').stop().fadeOut(222);
+		}
+	}).scroll();
+});
+
 var cookcnt=0
 $(document).ready(function()
 {
