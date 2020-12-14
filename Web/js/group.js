e22ca007c3186f801b5a1e19647a6ef51543b58f
OscarChang1380 <st94280@gmail.com>
2020-12-15 01:07:23 +0800

    12/15

--- Web/js/group.js
+++ Web/js/group.js
@@ -633,10 +633,13 @@ function Creategroup(){
 						data : JSON.stringify(data),
 						contentType : "application/json;charset=utf-8",
 						success: function(msg){
+							var delayInMilliseconds = 2000;
 							if(msg=="success")
 							{
 								swal("創建成功!", "快邀請朋友加入你的社團吧", "success");
-								location.reload();
+								setTimeout(function() {
+									location.reload();
+								}, delayInMilliseconds);
 							}
 						}
 					});
