fd2161cdaf5e7322b328b13321784ffd29a37b42
陳君平 <daniel9563769@gmail.com>
2020-12-17 00:23:35 +0800

    12/16

--- api/main.js
+++ api/main.js
@@ -425,7 +425,7 @@ app.post('/api/show_profile',function(req,res){
     //console.log(cook_art);
     var art_text_sql = 'select article.article_text,article.article_id,article.article_picture,user_info.user_name,likes.like_id,article.likes,article.club_id\
                         from user_info,article left join likes on likes.article_id = article.article_id\
-                        where article.club_id = \"'+ gid +'\" and article.post_level = "2"\
+                        where article.club_id = \"'+ gid +'\" and article.post_level = "2" and user_info.user_id = article.user_id\
                         order by article.article_time desc limit '+cook_art+',10';
     con.query(art_text_sql,function(err,result){
         if(err) throw err;
