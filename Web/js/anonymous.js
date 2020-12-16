2910ffe21a3662ac7ad39b3915058a95efb3f0b8
陳君平 <daniel9563769@gmail.com>
2020-12-16 18:30:25 +0800

    12/16

--- Web/js/anonymous.js
+++ Web/js/anonymous.js
@@ -154,7 +154,7 @@ $(document).ready(function()
 	});
 	
 	$(window).scroll(function(){
-		var totalheight = $("body").height() - $(window).height();
+		var totalheight = $("body").height() - $(window).height()-50;
 		if($(window).scrollTop()>=totalheight)
 		{
 			add_anmsarticle();
